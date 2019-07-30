dba57e56e7b7287f6f026645f1bb1d41cdc0a6dc
Daniel Welsh <danjwelsh24@gmail.com>
2019-07-30 18:38:58 +0200

    feat(leaderboards): adding member cards to leaderboards :sparkles:

--- src/views/Leaderboard.vue
+++ src/views/Leaderboard.vue
@@ -5,6 +5,15 @@
         section.section
             div.container
                 h1.title {{boardData.leaderboard.name}}
+                div.member-container
+                    div.card.member-card(
+                        v-for="member in boardData.members"
+                    )
+                        h4.is-size-5 {{ member.displayName }}
+                        p 
+                            span.icon
+                                i.fas.fa-coffee
+                                | &nbsp;{{ member.coffees.length }}
 </template>
 
 <script>
@@ -40,3 +49,51 @@ export default {
   }
 };
 </script>
+
+<style lang="scss" scoped>
+.member-container {
+  display: grid;
+  grid-template-columns: repeat(1, 1fr);
+  grid-gap: 10px;
+  grid-auto-rows: minmax(100px, auto);
+
+  margin-bottom: 1.25%;
+}
+
+// mobile
+@media (min-width: 500px) {
+  .member-container {
+    grid-template-columns: repeat(2, 1fr);
+  }
+}
+
+// tablet
+@media (min-width: 768px) {
+  .member-container {
+    grid-template-columns: repeat(5, 1fr);
+  }
+}
+
+.member-card {
+  display: inline-grid;
+
+  padding: 5%;
+  display: flex;
+  flex-direction: column;
+  box-shadow: 0 1px 6px 0 rgba(0, 0, 0, 0.25);
+
+  transition-duration: 0.4s;
+
+  &:hover {
+    box-shadow: 0 1px 10px 0 rgba(0, 0, 0, 0.5);
+  }
+
+  .is-size-5 {
+    flex-grow: 1;
+  }
+
+  .icon {
+    margin-left: 2.5%;
+  }
+}
+</style>
